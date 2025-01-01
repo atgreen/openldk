2e71ea4f696d749fc4e35ccef09fa09e360732fa
Anthony Green <green@moxielogic.com>
2025-01-01 10:06:40 -0500

    feat(testsuite): add new java test file 'ni.java'

---
+++ testsuite/aaa/ni.java
@@ -0,0 +1,27 @@
+/* { dg-output "^Hello!" } */
+
+package aaa;
+
+import java.util.Date;
+
+public class ni {
+
+   public static void main(String[] args) {
+
+      try {
+         // date object
+         Date d = new Date();
+         Class cls = d.getClass();
+         System.out.println("Time = " + d.toString());
+
+         /* creates a new instance of the class represented by this
+            Class object cls */
+         Object obj = cls.newInstance();
+         System.out.println("Time = " + obj);
+      } catch(InstantiationException e) {
+         System.out.println(e.toString());
+      } catch(IllegalAccessException e) {
+         System.out.println(e.toString());
+      }
+   }
+}
