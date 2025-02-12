dc32574560e33abfb0015e33e9e4c3aeae641951
Anthony Green <green@moxielogic.com>
2025-02-12 08:10:07 -0500

    fix(codegen): correct the class instantiation in <expression>

--- src/codegen.lisp
+++ src/codegen.lisp
@@ -341,7 +341,7 @@
     (make-instance '<expression>
                    :insn insn
                    :code `(let ((lookup (make-instance '|java/lang/invoke/MethodHandles$Lookup|)))
-                            (|<init>(Ljava/lang/Class;)| lookup (class insn))
+                            (|<init>(Ljava/lang/Class;)| lookup ,class)
                             (print lookup)
                             (error "unimplemented")))))
 
