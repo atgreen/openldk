7620b28393774aaef4200db823bb5be3db12d8c1
Anthony Green <green@moxielogic.com>
2025-02-28 06:28:16 -0500

    style(testsuite): remove trailing spaces and update print statements in StackTrace2.java

--- testsuite/gcj/StackTrace2.java
+++ testsuite/gcj/StackTrace2.java
@@ -3,8 +3,8 @@
 
 public class StackTrace2
 {
-  public static void main(String[] args) 
-  { 
+  public static void main(String[] args)
+  {
     try
     {
       new StackTrace2().a();
@@ -16,24 +16,24 @@ public class StackTrace2
     }
   }
 
-  void a() 
-  { 
+  void a()
+  {
     new Inner().foo();
   }
-  
+
   class Inner
   {
     public void foo()
     {
       doCrash(null);
-    }  
+    }
 
     public void doCrash(Object o)
     {
       o.toString();
     }
-  }  
-  
+  }
+
   static void checkTrace(StackTraceElement[] trace)
   {
     System.out.println("Trace length = " + trace.length);
@@ -42,28 +42,28 @@ public class StackTrace2
     checkLine(trace[2], "StackTrace2", "a", 21);
     checkLine(trace[3], "StackTrace2", "main", 10);
   }
-  
-  static void checkLine(StackTraceElement frame, String expected_cl, 
+
+  static void checkLine(StackTraceElement frame, String expected_cl,
                 	String expected_method, int expected_line)
   {
     if (frame.getClassName().equals(expected_cl))
-      System.out.print(expected_cl);
+      System.out.println(expected_cl);
     else
-      System.out.print("FAIL - expected " + expected_cl + ", got: " + 
+      System.out.println("FAIL - expected " + expected_cl + ", got: " +
 		       frame.getClassName());
-    
+
     System.out.print(".");
 
     if (frame.getMethodName().equals(expected_method))
-      System.out.print(expected_method);
+      System.out.println(expected_method);
     else
-      System.out.print("FAIL - expected " + expected_method + ", got: " +
+      System.out.println("FAIL - expected " + expected_method + ", got: " +
 		       frame.getMethodName());
 
     System.out.print(":");
-    
+
     // Permit either the correct line number or no line number. This is so
-    // we don't fail on platforms that don't yet support reading debug info 
+    // we don't fail on platforms that don't yet support reading debug info
     // for stack traces, or when no debug info is available.
     if (frame.getLineNumber() < 0
         || (frame.getLineNumber() == expected_line
